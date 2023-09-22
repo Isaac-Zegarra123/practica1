6e82632bbf25b0d39d845d3bc335ac6a2068cdf3
Isaac-Zegarra123 <isaacrodrigozegarramamani@gmail.com>
2023-09-21 21:26:09 -0400

    cambios

--- ejercicio1.js
+++ ejercicio1.js
@@ -3,4 +3,7 @@ function square(numero) {
     var cuadrado = numero * numero;
     return cuadrado; 
 }
+var numero = 5;
+var resultado = square(numero);
+console.log("El cuadrado de " + numero + " es " + resultado);
 
